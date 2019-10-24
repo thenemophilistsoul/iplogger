0914e9045f2b9e774043a797cd0501f0a0bbe159
Tibin Sunny <33937416+Tibinsunny@users.noreply.github.com>
2019-10-24 16:43:34 +0530

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,6 +1,8 @@
 
 <?php
-$ip = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR
+$date = date("l d F H:i:s");
+$browser = $_SERVER['HTTP_USER_AGENT'];
 
 $fp = fopen("ips.txt", "a");
 fputs($fp, "IP: $ip\n  Date: $date\n    User Agent: $browser\n\n\n");
